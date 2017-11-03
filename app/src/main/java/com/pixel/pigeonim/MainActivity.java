e4ad940c9c30707dcbf14b7d3580dcb806714642
wongchen <wongchen1992@gmail.com>
2017-11-03 19:32:59 +0800

    test by:wongchen

--- app/src/main/java/com/pixel/pigeonim/MainActivity.java
+++ app/src/main/java/com/pixel/pigeonim/MainActivity.java
@@ -3,11 +3,17 @@ package com.pixel.pigeonim;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.pixel.pigeonim.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void init() {
+
     }
 }

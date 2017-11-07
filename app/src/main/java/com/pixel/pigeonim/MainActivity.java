57239486a92bad436a3afc86288f971d072ac1d1
wongchen <wongchen1992@gmail.com>
2017-11-07 14:15:49 +0800

    登录布局

--- app/src/main/java/com/pixel/pigeonim/MainActivity.java
+++ app/src/main/java/com/pixel/pigeonim/MainActivity.java
@@ -1,8 +1,39 @@
 package com.pixel.pigeonim;
 
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.pixel.pigeonim.activity.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.et_account)
+    EditText etAccount;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.tv_find_password)
+    TextView tvFindPassword;
+    @BindView(R.id.tv_sign_up)
+    TextView tvSignUp;
+    @BindView(R.id.btn_sign_in)
+    Button btnSignIn;
+    @BindView(R.id.text_input_layout_account)
+    TextInputLayout textInputLayoutAccount;
+    @BindView(R.id.text_input_layout_password)
+    TextInputLayout textInputLayoutPassword;
 
     @Override
     public int getLayoutId() {
@@ -11,6 +42,69 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void init() {
-;
+        etAccount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!TextUtils.isEmpty(etAccount.getText().toString()))
+                    textInputLayoutAccount.setErrorEnabled(false);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        etPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!TextUtils.isEmpty(etPassword.getText().toString()))
+                    textInputLayoutPassword.setErrorEnabled(false);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @OnClick({R.id.tv_find_password, R.id.tv_sign_up, R.id.btn_sign_in})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_find_password:
+
+                break;
+            case R.id.tv_sign_up:
+                break;
+            case R.id.btn_sign_in:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        String account = etAccount.getText().toString();
+        String password = etPassword.getText().toString();
+        if (TextUtils.isEmpty(account)) {
+            textInputLayoutAccount.setError("账户不能为空");
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            textInputLayoutPassword.setError("密码不能为空");
+            return;
+        }
+
     }
 }

f90d656dcd0bc9ee3d38d39eb21ccc0935690db5
Batman <250746335@qq.com>
2017-11-08 18:21:25 +0800

    heihei

--- app/src/main/java/com/pixel/pigeonim/activity/SignInActivity.java
+++ app/src/main/java/com/pixel/pigeonim/activity/SignInActivity.java
@@ -93,7 +93,7 @@ public class SignInActivity extends BaseActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_find_password:
-
+                startActivity(PassWordFindActivity.class);
                 break;
             case R.id.tv_sign_up:
                 startActivity(SignUpActivity.class);
